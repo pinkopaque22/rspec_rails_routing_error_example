69e3b976b67ec50aa8d8b805261412f7f35841d8
Kevin McPhillips <github@kevinmcphillips.ca>
2013-02-06 13:42:09 -0600

    Resolved, but it may indicate a regression somewhere

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 RspecRailsRoutingErrorExample::Application.routes.draw do
 
   resources :payments, only: [:index] do
+    ## This works in the app, but fails in rspec
     resources :payment_policies, only: [:index], controller: "Payments::PaymentPolicies"
+
+    ## Either of these work in rspec and in the app
+    # resources :payment_policies, only: [:index], controller: "payments/payment_policies"
+    # resources :payment_policies, only: [:index], module: :payments
   end
 
 end
